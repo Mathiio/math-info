b773ddac6e8932de7ccf918541372802db7b871f
Marianne Kerckhove <marianne3002@outlook.fr>
2023-11-19 15:23:17 +0100

    add : projective geometry

--- src/Conics.hpp
+++ src/Conics.hpp
@@ -2,5 +2,5 @@
 #include <Eigen/Dense>
 #include <vector>
 
-Eigen::VectorXd conicCoefficients(std::vector<Eigen::VectorXd> points);
+Eigen::VectorXd conicCoefficients(std::vector<Eigen::VectorXd> points, bool isHomogeneous);
 Eigen::VectorXd pointTangent(Eigen::VectorXd point, Eigen::VectorXd conic);
